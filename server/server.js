ffde348c27c9e10758571ceaafcd7d72ee80d5d0
Guster <spavle.gh@gmail.com>
2023-12-16 19:31:43 +0100

    socket pokusaj prvi

--- server/server.js
+++ server/server.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const redis = require("redis");
+const http = require("http");
+const Server = require("socket.io");
 
 const app = express();
 const port = 3001;
 
-let client_id = 0;
-let subClients = [];
-let pubClients = [];
+const server = http.createServer(app);
+const io = Server(server);
+
+const cli = redis.createClient();
+cli.connect();
 
 app.get('/',(req,res) => {
-
     res.send("root");
+})
 
+io.on('connect', (socket) =>{
+    console.log("User connected");
+    io.on('disconnect',() => {
+        console.log("User disconnected");
+    })
+})
+
+app.post("/createChannel", (req,res) => {
+    //let channel_name = req.body.channelName;
+    //let player_name = req.body.playerName;
+
+    let ret = cli.get("activeChannels",(err,reply) => {
+        console.log(reply);
+    });
+
+    //console.log(ret);
+
+    res.status(200).send({message: ret});
 })
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
+import { useState } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
+import { socket } from './Socket';
 
 
 
-let option = "main";
 
-function Main(){
-
-  function changeOption(opt){
-    console.log(opt);
-  }
-
-  return(
-    <>
-      <button onClick={() => {
-        changeOption("Kurcina")
-      }}>
-        Create channel
-      </button>
-      <button>
-        Join channel
-      </button>
-    </>
-  );
-}
 
 function Menu(){
 
+  //fetch fun
+
+  return(
+    <div className='menu-div'>
+
+      <div>
+        <p>Username</p>
+        <input></input>
+        <p>Channel name</p>
+        <input></input>
+      </div>
+
+      <div className='menu-buttons-div'>
+      <button onClick={() => {
+        //fetch call
+      }}>
+        Create channel
+      </button>
+      <button onClick={() => {
+        //fetch call
+      }}>
+        Join channel
+      </button>
+      </div>
+
+    </div>
+  );
 }
 
 function App() {
-
+  
 
   return (
-    <Main/>
+    <Menu/>
   );
 }
 
